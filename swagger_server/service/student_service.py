9dea30e58d87f9911a9ecca052248f6719d23424
Anandan Krishnsamy <44828456+anandanlk@users.noreply.github.com>
2024-02-16 21:13:00 +0100

    MongoDB

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -1,41 +1,28 @@
 import os
-import tempfile
-from functools import reduce
 
-from tinydb import TinyDB, Query
+from pymongo import MongoClient
 
-db_dir_path = tempfile.gettempdir()
-db_file_path = os.path.join(db_dir_path, "students.json")
-student_db = TinyDB(db_file_path)
+# Connecting to Database
+mongo_db = MongoClient(os.getenv('MONGO_URI'))
 
 
 def add(student=None):
-    queries = []
-    query = Query()
-    queries.append(query.first_name == student.first_name)
-    queries.append(query.last_name == student.last_name)
-    query = reduce(lambda a, b: a & b, queries)
-    res = student_db.search(query)
-    if res:
+    if mongo_db.local.student.find_one({"first_name": student.first_name, "last_name": student.last_name}):
         return 'already exists', 409
-
-    doc_id = student_db.insert(student.to_dict())
-    student.student_id = doc_id
+    result = mongo_db.local.student.insert_one(student.to_dict())
     return student.student_id
 
 
 def get_by_id(student_id=None, subject=None):
-    student = student_db.get(doc_id=int(student_id))
+    student = mongo_db.local.student.find_one({"student_id": student_id})
+    del student["_id"]
     if not student:
         return 'not found', 404
-    student['student_id'] = student_id
-    print(student)
     return student
 
 
 def delete(student_id=None):
-    student = student_db.get(doc_id=int(student_id))
-    if not student:
+    result = mongo_db.local.student.find_one_and_delete({"student_id": student_id})
+    if not result:
         return 'not found', 404
-    student_db.remove(doc_ids=[int(student_id)])
     return student_id
