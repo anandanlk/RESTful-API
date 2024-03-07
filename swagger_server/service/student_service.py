eba8872b76368db453acf4b8102d4af629cb1c5c
Anandan Krishnsamy <44828456+anandanlk@users.noreply.github.com>
2024-03-07 13:27:25 +0100

    Update student_service.py

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -10,7 +10,7 @@ def add(student=None):
     if mongo_db.local.student.find_one({"first_name": student.first_name, "last_name": student.last_name}):
         return 'already exists', 409
     result = mongo_db.local.student.insert_one(student.to_dict())
-    return result.student_id
+    return student.student_id
 
 
 def get_by_id(student_id=None):
