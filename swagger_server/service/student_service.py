d41147675192c05916f7e62d0f930a241d76fcc0
Anandan Krishnsamy <44828456+anandanlk@users.noreply.github.com>
2024-03-07 17:01:08 +0100

    Multiple changes

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -11,20 +11,20 @@ def add(student=None):
     if mongo_db.local.student.find_one({"first_name": student.first_name, "last_name": student.last_name}):
         return 'already exists', 409
     result = mongo_db.local.student.insert_one(student.to_dict())
-    return str(student), 200
+    return str(result.inserted_id), 200
 
 
-def get_by_id(student_id=None):
-    student = mongo_db.local.student.find_one({"student_id": student_id})
+def get_by_id(student_id=None, subject=None):
+    student = mongo_db.local.student.find_one({"_id": ObjectId(student_id)})
     if not student:
         return 'not found', 404
-    # student['student_id'] = student_id
+    student['student_id'] = str(student['_id'])
     del student["_id"]
     return student
 
 
 def delete(student_id=None):
-    result = mongo_db.local.student.find_one_and_delete({"student_id": student_id})
+    result = mongo_db.local.student.find_one_and_delete({"_id": ObjectId(student_id)})
     if not result:
         return 'not found', 404
     return student_id
